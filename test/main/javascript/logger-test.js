041473ba775c33fd9fab39f835a2d5735bc3b42d
John Paul <john@johnkpaul.com>
2012-04-11 16:52:59 -0400

    add logger-test file

---
+++ test/main/javascript/logger-test.js
@@ -0,0 +1,23 @@
+/*global module:false, test:false, ok:false */
+(function($,window, undefined){
+
+    module("Goose Log error handling",{
+        "setup":function(){
+
+        }
+    });
+
+    test("example passing test", 1, function(){
+        ok(true);
+
+    });
+
+    test("when error is thrown to window, Goose Log stores error", function(){
+
+    });
+
+
+}(window.jQuery,window));
+
+
+
